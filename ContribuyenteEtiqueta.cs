a1f341e4687af9cccc5c126febe9951bcf32cfeb
Ericson <efriasm@gmail.com>
2025-01-08 18:58:13 -0400

    Actualiza referencia label NombreComercial

--- ContribuyenteEtiqueta.cs
+++ ContribuyenteEtiqueta.cs
@@ -3,7 +3,7 @@
     public class ContribuyenteEtiqueta
     {
         public string Nombre { get; set; } = "Nombre/Razón Social";
-        public string NombreComercial { get; set; } = "Nombre/Razón Social";
+        public string NombreComercial { get; set; } = "Nombre Comercial";
         public string Estado { get; set; } = "Estado";
     }
 }
